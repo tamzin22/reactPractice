b16bc33464788de1fb7d0123d8f52ea5af51569e
tamzin22 <evelynetamzin@gmail.com>
2023-11-12 14:56:06 +0300

    Event Handler

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
 import EventBinding from './components/EventBinding';
+import ParentComponent from './components/ParentComponent';
 
 function App() {
   return (
     <div className="App">
        {/* <Message /> */}
-       <Counter />
+       {/* <Counter />
        <FunctionClick />
        <ClassClick />
-       <EventBinding />
+       <EventBinding /> */}
+       <ParentComponent />
         {/* <Practice  name = 'Kingsley'>
           <p>This is A Props Practice</p>
         </Practice>
